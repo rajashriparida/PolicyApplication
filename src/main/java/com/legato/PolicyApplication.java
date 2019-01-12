954c304977c005fa6bfd0ccdae5200bd1472e403
paridarajashree <paridarajashree14@gmail.com>
2019-01-12 23:38:04 +0530

    added repository

---
+++ src/main/java/com/legato/PolicyApplication.java
@@ -0,0 +1,14 @@
+package com.legato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyApplication.class, args);
+	}
+
+}
+
